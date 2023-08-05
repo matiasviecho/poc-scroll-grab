6f07144fd893490643499e6f547c4bea66d13a20
Matias Viecho <mpviecho@gmail.com>
2023-08-05 16:59:45 -0300

    feat(list): add onClick scroll with jsx

--- components/Item/Item.jsx
+++ components/Item/Item.jsx
@@ -1,3 +1,23 @@
-export const Item = () => {
-  return <>item</>;
+import style from './Item.module.scss';
+
+export const Item = ({ number, title, body }) => {
+  if (!title && !body) return null;
+  return (
+    <div
+      className={`${style['item-container']} ${
+        number === 50 ? style['fifty-test'] : ''
+      }`}
+    >
+      {title && (
+        <header>
+          <span>{title}</span>
+        </header>
+      )}
+      {body && (
+        <main>
+          <span>{body}</span>
+        </main>
+      )}
+    </div>
+  );
 };
--- components/List/List.jsx
+++ components/List/List.jsx
@@ -1,15 +1,42 @@
 'use client';
+import { useRef, useState } from 'react';
 import { Item } from '../Item/Item';
 import styles from './List.module.scss';
 export const List = ({ items }) => {
-  console.log('items:', items);
+  const [active, isActive] = useState(false);
+  const pointerMove = useRef();
+  const ref = useRef();
   if (!items || items.length === 0) return <div>No hay elementos</div>;
+  const handlePointerMove = (e) => {
+    if (!active) return;
+    //console.log('e:', e);
+    //console.log('e.nativeEvent.offsetX:', e.nativeEvent.offsetX);
+    const targetScrollLeft =
+      ref.current.scrollLeft - e.nativeEvent.offsetX + 100;
+    const updateScroll = () => {
+      ref.current.scrollLeft = targetScrollLeft;
+    };
+
+    requestAnimationFrame(updateScroll);
+  };
   return (
-    <ul className={styles['list']}>
+    <ul
+      className={styles['list']}
+      onPointerDown={(e) => {
+        isActive(true);
+      }}
+      onPointerUp={() => isActive(false)}
+      onPointerMove={(e) => handlePointerMove(e)}
+      ref={ref}
+    >
       {items.map((item, index) => {
         return (
           <li key={item.id || index + 100}>
-            <Item content={item} />
+            <Item
+              number={index}
+              title={item.title}
+              body={item.body}
+            />
           </li>
         );
       })}
