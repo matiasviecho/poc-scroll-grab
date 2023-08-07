352713ce83e54b9c93a8f26a12031981870e3b16
Matias Viecho <mpviecho@gmail.com>
2023-08-07 20:03:17 -0300

    refactor(build): add deploy yml

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import { List } from '@/components/List/List';
 import styles from './page.module.scss'
 import Link from 'next/link';
-
+import Image from 'next/image';
 export default async function Home({searchParams: {use = null}}) {
   const fetchData = async () => {
     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -20,6 +20,14 @@ export default async function Home({searchParams: {use = null}}) {
       <footer>        
        {!isBig  ? <Link href={'/?use=big'}>go bigger</Link> : <Link href={'/'}>go smaller</Link>}
       </footer>
+      <Image
+     src="/nextjs-github-pages/vercel.svg"
+     alt="Vercel Logo"
+     className={styles.vercelLogo}
+     width={100}
+     height={24}
+     priority
+   />
     </section>
   )
 }
